69e843d382aa747b497e007e4a2e628c075ca223
Bendy Zhang <zb@bndy.net>
2018-12-06 21:36:26 +0800

    pring deprecation details

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,6 +1,9 @@
 const merge = require('webpack-merge');
 const globalConfig = require('./webpack.config.js');
 
+// printing warning details to catch where throw it
+process.traceDeprecation = true;
+
 module.exports = merge(globalConfig, {
     mode: 'development',
     devtool: 'inline-source-map',
