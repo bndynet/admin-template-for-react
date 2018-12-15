64ad4c2aa1cfb556fa7931a57b3c930c0127d8d8
Bendy Zhang <zb@bndy.net>
2018-12-15 10:33:36 +0800

    configuration for production

--- src/config.tsx
+++ src/config.tsx
@@ -1,8 +1,20 @@
-const config = {
-    clientId: '500bd7533a',  //'188c0da703',
+import _merge from 'lodash-es/merge';
+
+let config = {
+    clientId: '500bd7533a',
     clientSecret: 'bc55228ce6c70e97f4ee',
-    oauthBaseURL: 'http://localhost:9110/', // 'http://cloud.bndy.net/service-sso/',
-    resourceBaseURL: 'http://localhost:8080/',
+    oauthBaseURL: 'http://localhost:9110/',
+    resourceBaseURL: '/',
 };
+
+switch(process.env.MODE_ENV) {
+    case 'production':
+        config = _merge(config, {
+            clientId: '188c0da703',
+            clientSecret: 'bc55228ce6c70e97f4ee',
+            oauthBaseURL: 'http://cloud.bndy.net/service-sso/',
+        });
+        break;
+}
 
 export default config;
