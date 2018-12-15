638d54e33cb0047ecf2dc58a93e60d7304aa2451
Bendy Zhang <zb@bndy.net>
2018-12-15 17:38:56 +0800

    change oauth endpoint

--- src/config/app.prod.tsx
+++ src/config/app.prod.tsx
@@ -3,7 +3,7 @@ import { AppConfig } from '.';
 const config: AppConfig = {
     clientId: '188c0da703',
     clientSecret: 'f3dd317369ae622113f0',
-    oauthBaseURL: 'http://cloud.bndy.net/service-sso/'
+    oauthBaseURL: 'https://cloud.bndy.net/service-sso/'
 };
 
 module.exports = config;
