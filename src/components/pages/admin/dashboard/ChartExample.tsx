c5715f1756f208f70667b6865c86bbb6f90946a1
Bendy Zhang <zb@bndy.net>
2019-01-20 00:00:29 +0800

    use @recharts-wrapper instead of native recharts

--- src/components/pages/admin/dashboard/ChartExample.tsx
+++ src/components/pages/admin/dashboard/ChartExample.tsx
@@ -1,43 +1,19 @@
 import * as React from "react";
-import { Chart } from "app/ui";
-import { Chart as Bart } from "@bndynet/recharts-wrapper/lib/cjs/src";
+import { Chart } from "@bndynet/recharts-wrapper";
 
-const data = [
-    { name: "Mon", Visits: 0, Orders: 20 },
-    { name: "Tue", Visits: 100, Orders: 0 },
-    { name: "Wed", Visits: 0, Orders: 430 },
-];
+const data = [{ name: "Mon", Visits: 0, Orders: 20 }, { name: "Tue", Visits: 100, Orders: 0 }, { name: "Wed", Visits: 0, Orders: 430 }];
 
 function loadData() {
     return new Promise<any[]>((resolve, reject) => {
         setTimeout(() => {
-            const response = [
-                { name: "Mon", Visits: 2200, Orders: 3400, ShoppingCart: 1210 },
-                { name: "Tue", Visits: 1280, Orders: 2398, ShoppingCart: 3000 },
-                { name: "Wed", Visits: 5000, Orders: 4300, ShoppingCart: 2300 },
-                { name: "Thu", Visits: 4780, Orders: 2908, ShoppingCart: 4500 },
-                { name: "Fri", Visits: 5890, Orders: 4800, ShoppingCart: 1000 },
-                { name: "Sat", Visits: 4390, Orders: 3800, ShoppingCart: 3400 },
-                { name: "Sun", Visits: 4490, Orders: 4300, ShoppingCart: 2300 },
-            ];
+            const response = [{ name: "Mon", Visits: 2200, Orders: 3400, ShoppingCart: 1210 }, { name: "Tue", Visits: 1280, Orders: 2398, ShoppingCart: 3000 }, { name: "Wed", Visits: 5000, Orders: 4300, ShoppingCart: 2300 }, { name: "Thu", Visits: 4780, Orders: 2908, ShoppingCart: 4500 }, { name: "Fri", Visits: 5890, Orders: 4800, ShoppingCart: 1000 }, { name: "Sat", Visits: 4390, Orders: 3800, ShoppingCart: 3400 }, { name: "Sun", Visits: 4490, Orders: 4300, ShoppingCart: 2300 }];
             resolve(response);
         }, 5000);
     });
 }
 
 function ChartExample() {
-    return (
-        <Bart
-            data={data}
-            xKey="name"
-            dataSource={loadData}
-            series={[
-                { key: "Visits", color: "#82ca9d", type: "bar" },
-                { key: "Orders", color: "#8884d8", type: "area" },
-                { key: "ShoppingCart", color: "#ff0000" },
-            ]}
-        />
-    );
+    return <Chart data={data} xKey="name" dataSource={loadData} series={[{ key: "Visits", color: "#82ca9d", type: "bar" }, { key: "Orders", color: "#8884d8", type: "area" }, { key: "ShoppingCart", color: "#ff0000" }]} loadingElement={<span>Loading...</span>} />;
 }
 
 export default ChartExample;
