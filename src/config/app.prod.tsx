dd0c54e640c04099886ed9ffd113f65610c8324f
Bendy Zhang <zb@bndy.net>
2019-04-07 17:21:43 +0800

    fix(config): OAuth scope can not be overwrote in prod

--- src/config/app.prod.tsx
+++ src/config/app.prod.tsx
@@ -1,11 +1,12 @@
+import _merge from "lodash-es/merge";
 import config = require("./app.oauth-code");
 
-module.exports = {
-    ...config,
+module.exports = _merge(config, {
     authConfig: {
         authorizationUri: "https://cloud.bndy.net/service-sso/oauth/authorize?response_type=code&client_id={clientId}&redirect_uri={callbackUri}&&scope={scope}",
         accessTokenUri: "https://cloud.bndy.net/service-sso/oauth/token?client_id={clientId}&client_secret={clientSecret}&grant_type=authorization_code&code={code}&redirect_uri={callbackUri}",
         userProfileUri: "https://cloud.bndy.net/service-sso/oauth/me",
         logoutUri: "https://cloud.bndy.net/service-sso/oauth/logout",
     },
-};
+});
+
